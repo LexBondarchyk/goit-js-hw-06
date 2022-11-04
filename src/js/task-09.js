b5ac4bdd35043207e4b6e984b8fd53ff131596f8
LexBondarchyk <108199972+LexBondarchyk@users.noreply.github.com>
2022-11-04 19:43:31 +0200

    fin

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,16 +1,15 @@
 
 
-
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-};
-const body = document.querySelector(".change-color");
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const changeButton = document.querySelector('button.change-color')
+const body = document.querySelector('body')
+const colorValue = document.querySelector('span.color')
 
-body.style.backgroundColor = getRandomHexColor();
-button.style.fontSize = "24px";
-button.style.textAlign = "center";
 
-console.log(button.style); // inline styles object
-
+changeButton.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+})
